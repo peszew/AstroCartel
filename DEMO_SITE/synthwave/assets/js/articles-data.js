2cbcdbe6bbeb95d29228a3f7f83b7cb84899a94a
piotr <piotr@astrocartel.site>
2026-03-06 14:54:12 +0100

    feat: add hire me section and business resources

---
+++ DEMO_SITE/synthwave/assets/js/articles-data.js
@@ -0,0 +1,91 @@
+// Language data for all articles
+const articles = {
+    "snitching": {
+        title: "Snitching",
+        artist: "Pop Smoke ft. Quavo, Future",
+        content: {
+            en: "RIP WOO! Duo Quavo Pop + Future with their usual awesome verse blows minds. Plus the beat, a jazz classic, personally reminds me of a good classic beat from Dr. Dre, which I think Seth The Chef & Buddah Bless, the producers of the track, were definitely inspired by. Probably my new phone ringtone (since the release of 21 Savage - Bank Account).",
+            es: "RIP WOO! El dúo Quavo Pop + Future con su habitual verso increíble vuela mentes. Además, el ritmo, un clásico del jazz, personalmente me recuerda a un buen ritmo clásico de Dr. Dre, en el que creo que Seth The Chef & Buddah Bless, los productores del tema, definitivamente se inspiraron. Probablemente mi nuevo tono de llamada (desde el lanzamiento de 21 Savage - Bank Account).",
+            pl: "RIP WOO! Duet Quavo Pop + Future z jak zwykle zajebistym wersem rozbija czacheee. Do tego bit, klasyka jazzu tzw. mi osobiście kojarzy się z dobrym klasycznym bitem od Dr. Dre, którym Seth The Chef & Buddah Bless, czyli producenci numeru, myślę, że na pewno się wzorowali. Chyba nowy dzwonek na telefon (od premiery 21 Savage - Bank Account)."
+        },
+        date: "2025-04-07",
+        image: "https://i.imgur.com/fkDjPpL.png",
+        link: "https://www.youtube.com/watch?v=BhbVY3DNYjk"
+    },
+    "bad-karma": {
+        title: "Bad Karma",
+        artist: "Axel Thesleff",
+        content: {
+            en: "Bonus article for April Fools' Day! When I was writing this article about the track, I had no idea about the background behind it. The track itself – a classic. Everyone has heard that characteristic mumbling and the deep bass coming in underneath at some point. It's been playing on my speakers from time to time since 2017. One of those tracks you forget about, and then it randomly comes back one day. Interestingly, it's the backstory that makes it even more fascinating. The producer Axel didn't earn anything from it! The sample was taken from some free-to-use beat pack, and the vocal is also a sample from Shamur – Let The Music Play, which actually sparked a bit of controversy. Anyway, I encourage you to listen to both tracks. Cheers!",
+            es: "¡Artículo extra por el Día de los Inocentes! Cuando escribí este artículo sobre el tema, no tenía ni idea del trasfondo que había detrás. El tema en sí – un clásico. Todo el mundo ha escuchado alguna vez ese característico murmullo y el bajo profundo entrando por debajo. Suena en mis altavoces de vez en cuando desde 2017. Es una de esas canciones que se te olvidan y un día vuelven por casualidad. Lo curioso es precisamente lo que hay detrás de esta canción. ¡El productor Axel no ganó nada con ella! El sample fue tomado de un paquete de beats gratuitos, y la voz también es un sample de Shamur – Let The Music Play, lo cual generó algo de polémica. De todas formas, te invito a escuchar ambos temas. ¡Saludos!",
+            pl: "Bonusowy artykuł w Prima Aprilis! Pisząc artykuł o tej nucie, w ogóle nie znałem background'u jaki za nią stoi. Sama nuta - klasyk. Każdy kiedyś usłyszał ten charakterystyczny bełkot i wjeżdżający pod niego głęboki basssss. Na głosnikach lata od czasu do czasu od 2017. Jedna z tych nutek, o której się zapomina i czasem kiedyś przypadkiem wróci. Co ciekawe właśnie z zaplecza tej nuty. Sam producent Axel, nie zarobił na niej nic! Sampel jest wzięty z jakiejś paczki beatów free to use, natomiast głos, to również sampel z Shamur - Let The Music Play, o który była lekka afera. W każdym razie zapraszam do odsłuchu jednego i drugiego utworu. Pozdro!"
+        },
+        date: "2025-04-01",
+        image: "https://i.imgur.com/22tseLe.png",
+        link: "https://soundcloud.com/axelthesleff/bad-karma"
+    },
+    "the-plan": {
+        title: "The Plan",
+        artist: "Wiz Khalifa (feat. Juicy J)",
+        content: {
+            en: "The very first lines already define the whole track: 'I looked in the mirror today And seen the realest person I ever met in my life.' A powerful reflection. Wiz and Juicy J — a rather unexpected collab — but the Memphis mad-style representative fit perfectly into Wiz's chill vibe. You're left with a clear head after listening. This track, the fifteenth from 2012's O.N.I.F.C., sees Wiz Khalifa and Juicy J discuss the unexpected but fulfilling natures of their respective rises to fame. Wiz raps about all that he has achieved despite doubters telling him that he couldn't, and their subsequent jealous hate. Juicy J talks about his crazy lifestyle but how it still goes hand-in-hand with thug realness.",
+            es: "Ya los primeros versos definen todo el tema: 'I looked in the mirror today And seen the realest person I ever met in my life.' Una reflexión poderosa. Wiz y Juicy J — una colaboración bastante inesperada — pero el representante del estilo loco de Memphis encajó perfectamente con el rollo relajado de Wiz. Terminas con la mente despejada después de escucharlo. Esta canción, la número quince del álbum O.N.I.F.C. de 2012, muestra a Wiz Khalifa y Juicy J hablando sobre lo inesperado pero gratificante que ha sido su camino hacia la fama. Wiz rapea sobre todo lo que ha logrado a pesar de los que dudaban de él y cómo ahora sienten envidia. Juicy J habla de su estilo de vida loco, pero cómo todavía mantiene su autenticidad callejera.",
+            pl: "Juz pierwsze wersy definiuja cały utwór: 'I looked in the mirror today And seen the realest person I ever met in my life.' Mocna rozkmina. Wiz i Juicy J, kolabo, dość niespodziewane, jednak do chill'u Wiza, reprezentant chorego stylu z Memphis wpasował się idealnie. Luźna głowa po odsłuchu. Ten utwór, piętnasty z albumu O.N.I.F.C. z 2012 roku, ukazuje jak Wiz Khalifa i Juicy J opowiadają o nieoczekiwanym, ale satysfakcjonującym przebiegu ich drogi do sławy. Wiz rapuje o wszystkim, co osiągnął, mimo że inni wątpili w jego możliwości i później zaczęli mu zazdrościć. Juicy J opowiada o swoim szalonym stylu życia, który mimo wszystko wciąż idzie w parze z uliczną autent ycznością."
+        },
+        date: "2025-03-30",
+        image: "https://i.imgur.com/VKP5fjG.png",
+        link: "https://soundcloud.com/noahbossmcman"
+    },
+    "y-que-fue": {
+        title: "Y Que Fue?",
+        artist: "Don Miguelo",
+        content: {
+            en: "LAMINE!!!!!!!!!!",
+            es: "LY19!!!!",
+            pl: "LY19 LEWAA"
+        },
+        date: "2025-03-24",
+        image: "https://i.imgur.com/kAdeGs5.png",
+        link: "https://soundcloud.com/veras-anthony/don-miguelo-y-que-fue"
+    },
+    "back-to-the-future": {
+        title: "Back To The Future",
+        artist: "A$AP Rocky",
+        content: {
+            en: "A song that was made during the 'Mouth Fulla Gold' sessions. The track was likely made in the same sessions as 'Purple Swag' and 'Peso'. Released online on September 19th, 2011 as a tribute to the famous Nike MAG shoes that were released on September 8th, 2011. These shoes were made in reference to the 1989 film 'Back to the Future Part II' as the shoes are a replica of the self-tying shoes in that movie. The song's title and hook reference the 'Back to the Future' film franchise saying 'Back to the future, that Marty McFly', who is one of the main characters in the film series.",
+            es: "Una canción que se hizo durante las sesiones de 'Mouth Fulla Gold'. La canción fue hecha probablemente en las mismas sesiones que 'Purple Swag' y 'Peso'. Lanzada en línea el 19 de septiembre de 2011 como homenaje a las famosas zapatillas Nike MAG lanzadas el 8 de septiembre de 2011. Dichas zapatillas se hicieron en referencia a la película de 1989 'Regreso al Futuro Parte II'. El título y el gancho hacen referencia a la franquicia 'Regreso al futuro' diciendo 'Back to the future, that Marty McFly', uno de los personajes principales de la serie.",
+            pl: "Zajebista nuta, nic dodać nic ująć."
+        },
+        date: "2025-03-16",
+        image: "https://i.imgur.com/pu2774C.png",
+        link: "https://soundcloud.com/basedbook-level-9/asap-rocky-back-to-the-future"
+    },
+    "need-to-feel-loved": {
+        title: "Need To Feel Loved",
+        artist: "Adam K & Soha (Vocal Mix)",
+        content: {
+            en: "Adam K & Soha's 'Need To Feel Loved' (Vocal Mix) masterfully blends emotional depth with progressive house beats, creating a track that resonates with the heart and the dance floor alike. OK, so this intro description was AI generated. This song is extremely good. It is simple, but not simple, after listening to it, you will know what I mean. The song samples 'Ghosts', part of the soundtrack to the 2002 film Road to Perdition. Imagine listening to this song while sitting in some pub with ocean view. Shisha type bar with drinks. 2/3 a.m. in the morning, naaah that's something else.",
+            es: "'Need To Feel Loved' (Vocal Mix) de Adam K & Soha combina magistralmente la profundidad emocional con ritmos house progresivos. OK, la descripción era AI. Esta canción es extremadamente buena. Es sencilla, pero no simple, después de escucharla, sabrás lo que quiero decir. La canción samplea 'Ghosts', parte de la banda sonora de la película de 2002 Road to Perdition. Imagínate escuchar esta canción mientras estás sentado en un pub con vistas al mar. Un bar tipo shisha con bebidas. 2/3 de la mañana, naaah eso es otra cosa.",
+            pl: "'Need To Feel Loved' (Vocal Mix) Adama K & Soha łączy mistrzowsko głębię emocjonalną z progresywnymi rytmami house. OK, koniec tego pieprzenia AI-owego. Zajebista nuta, sampel 'Ghosts' użyty w filmie z T. Hanks'em 'Road to Perdition'. Wyobraź sobie słuchanie tej nutki siedząc w jakimś pubie z widokiem na ocean. Shisha bar, driny o 2/3 w nocy, this hits different."
+        },
+        date: "2025-03-07",
+        image: "https://i.imgur.com/2Nw10zY.jpeg",
+        link: "https://soundcloud.com/search?q=need%20to%20feel%20loved"
+    }
+};
+
+// Language switching functionality
+let currentLanguage = localStorage.getItem('language') || 'en';
+
+function setLanguage(lang) {
+    currentLanguage = lang;
+    localStorage.setItem('language', lang);
+    location.reload();
+}
+
+function getContent(article, field) {
+    if (typeof article[field] === 'object') {
+        return article[field][currentLanguage] || article[field]['en'];
+    }
+    return article[field];
+}
